d60a6aec8bea986178629df69595146f69e37a67
YongYeon Kim <assavictory@gmail.com>
2023-12-21 11:45:00 +0900

    수업시 활용할 공통 모델, 레포지토리 셋팅

---
+++ lib/00_repository/star_repository.dart
@@ -0,0 +1,25 @@
+import '../00_model/star.dart';
+
+class StarRepository {
+  final List<Star> stars = [
+    Star(
+      title: '아이유',
+      subTitle: '아이유는 아이가 아니에요',
+      imageUrl:
+          'https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp',
+      description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    ),
+    Star(
+      title: '권은비',
+      subTitle: '워터밤 안 보셨나보네요',
+      imageUrl:
+          'https://cdn.asiaa.co.kr/news/photo/202307/132090_146830_4051.jpg',
+      description:
+          'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+    )
+  ];
+  List<Star> getStars() {
+    return stars;
+  }
+}
