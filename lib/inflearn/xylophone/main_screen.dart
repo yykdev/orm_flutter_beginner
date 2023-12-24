4012631926d782458b9ddd6f44319651ad36bcb0
YongYeon Kim <assavictory@gmail.com>
2023-12-25 02:24:20 +0900

    실로폰 사운드 라이브러리 셋팅 및 연결

--- lib/inflearn/xylophone/main_screen.dart
+++ lib/inflearn/xylophone/main_screen.dart
@@ -1,8 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
   const XylophoneApp({super.key});
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  Soundpool pool = Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
+
+  List<int> _soundIds = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initSoundPool();
+  }
+
+  Future<void> initSoundPool() async {
+    int soundId = await rootBundle
+        .load('assets/sound/do1.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/re.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/mi.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/fa.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/sol.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/la.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/si.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sound/do2.wav')
+        .then((soundData) => pool.load(soundData));
+    _soundIds.add(soundId);
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,55 +78,62 @@ class XylophoneApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text('실로폰'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: gunban('도', Colors.red),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            child: gunban('레', Colors.orange),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: gunban('미', Colors.deepOrangeAccent),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: gunban('파', Colors.cyan),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 48.0),
-            child: gunban('솔', Colors.blue),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 56.0),
-            child: gunban('라', Colors.purple),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: gunban('시', Colors.green),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 72.0),
-            child: gunban('도', Colors.red),
-          ),
-        ],
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: gunban('도', Colors.red, _soundIds[0]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: gunban('레', Colors.orange, _soundIds[1]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  child: gunban('미', Colors.deepOrangeAccent, _soundIds[2]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40.0),
+                  child: gunban('파', Colors.cyan, _soundIds[3]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48.0),
+                  child: gunban('솔', Colors.blue, _soundIds[4]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 56.0),
+                  child: gunban('라', Colors.purple, _soundIds[5]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 64.0),
+                  child: gunban('시', Colors.green, _soundIds[6]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 72.0),
+                  child: gunban('도', Colors.red, _soundIds[7]),
+                ),
+              ],
+            ),
     );
   }
 
-  Widget gunban(String text, Color color) {
-    return Container(
-      width: 50,
-      height: double.infinity,
-      color: color,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(color: Colors.white),
+  Widget gunban(String text, Color color, int soundId) {
+    return GestureDetector(
+      onTap: () {
+        pool.play(soundId);
+      },
+      child: Container(
+        width: 50,
+        height: double.infinity,
+        color: color,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import soundpool_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
 }
