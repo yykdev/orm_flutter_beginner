081be5d07c744b3eca2c4364a02f5732d1db1981
YongYeon Kim <assavictory@gmail.com>
2023-12-25 01:47:13 +0900

    sensor_plus 로 가속도 센서 값 얻기

--- lib/inflearn/tilt_sensor3/main_screen.dart
+++ lib/inflearn/tilt_sensor3/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class SensorApp extends StatefulWidget {
   const SensorApp({super.key});
@@ -16,18 +17,32 @@ class _SensorAppState extends State<SensorApp> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
-                shape: BoxShape.circle,
-              ),
-              width: 100,
-              height: 100,
-            ),
-          ),
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEvents,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final event = snapshot.data!;
+                List<double> accelerometerValues = [event.x, event.y, event.z];
+                print(accelerometerValues);
+
+                return Positioned(
+                  left: centerX,
+                  top: centerY,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                );
+              }),
         ],
       ),
     );
